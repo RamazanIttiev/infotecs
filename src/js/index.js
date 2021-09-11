8ea8bfaaacaeee2af89cbe145db86b6a816adb55
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-11 22:52:12 +0300

    toggleColumns component

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import { people } from './mock.js';
 import generateThead from './thead.js';
 import generateTbody from './tbody.js';
 import generateFilter from './filter.js';
+import toggleColumns from './toggleColumns.js';
 
 const table = document.querySelector('table');
 const data = Object.keys(people[0]).slice(1);
 generateFilter(table, data);
 generateThead(table, data);
 generateTbody(table, people);
+toggleColumns();
---
+++ src/js/toggleColumns.js
@@ -0,0 +1,29 @@
+const toggleColumns = data => {
+  const checkboxes = document.getElementsByClassName('checkbox');
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('click', e => {
+      const peopleTd = document.getElementsByClassName(e.target.id);
+
+      if (checkboxes[i].value === 'hide') {
+        for (let z = 0; z < peopleTd.length; z++) {
+          console.log(peopleTd[z]);
+          peopleTd[z].classList.add('hide');
+        }
+        document.getElementsByClassName(`${e.target.id}_head`)[0].classList.add('hide');
+        document.getElementsByClassName(`${e.target.id}_input`)[0].classList.add('hide');
+        checkboxes[i].value = 'show';
+      } else {
+        for (let z = 0; z < peopleTd.length; z++) {
+          console.log(peopleTd[z]);
+          peopleTd[z].classList.remove('hide');
+        }
+        document.getElementsByClassName(`${e.target.id}_head`)[0].classList.remove('hide');
+        document.getElementsByClassName(`${e.target.id}_input`)[0].classList.remove('hide');
+        checkboxes[i].value = 'hide';
+      }
+    });
+  }
+};
+
+export default toggleColumns;
