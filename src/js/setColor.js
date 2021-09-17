38b627eb9f86a9f40a47d2dd58f7031e68df17ce
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-17 12:12:03 +0300

    generateCellColor function fix

--- src/js/setColor.js
+++ src/js/setColor.js
@@ -4,9 +4,6 @@
 const generateCellColor = () => {
   const eyeColor = document.getElementsByClassName('eyeColor__col');
 
-  const colorDiv = document.createElement('div');
-  colorDiv.classList.add('eye__color');
-
   for (let i = 0; i < eyeColor.length; i++) {
     eyeColor[i].style.borderRight = `1px solid ${eyeColor[i].innerText}`;
   }
