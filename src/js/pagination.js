4c1b4b9386f6abebd0c57d9624289516297088bc
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-20 19:10:36 +0300

    initPagination comments

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,3 +1,15 @@
+/**
+ *
+ * @param {*} param0 state
+ * @param {*} callback
+ * @returns элемент пагинации
+ *
+ * pageCount определяет количество кнопок пагинации (окргление в большую сторону)
+ *
+ * Прохожусь по количеству кнопок и вызываю функцию для отрисовки отдельной кнопки (paginationButton)
+ *
+ *
+ */
 export const initPagination = ({ data, rowsPerPage, currentPage, hiddenColumns }, callback) => {
   const paginationElement = document.getElementById('pagination');
 
@@ -11,6 +23,27 @@ export const initPagination = ({ data, rowsPerPage, currentPage, hiddenColumns }
   return paginationElement;
 };
 
+/**
+ *
+ * @param {*} page Номер страницы
+ * @param {*} data Массив данных (mock)
+ * @param {*} rowsPerPage Строк на 1 страницу
+ * @param {*} currentPage Исходная страница
+ * @param {*} hiddenColumns Скрытые колонки
+ * @param {*} callback
+ * @returns Кнопку
+ *
+ * Создается кнопка
+ *
+ * Далее идет проверка, есил текущая страница совпадает с исходной, то кнопке добовляется активный класс
+ *
+ * При клике на кнопку очищаем чекбоксы
+ * Присваиваем значение текущей страницы исходной
+ *
+ * Меняем класс активности при клике на другую кнопку
+ *
+ * И вызываем колбэк, который генерирует новую таблицу с обновленными строками
+ */
 const paginationButton = (page, data, rowsPerPage, currentPage, hiddenColumns, callback) => {
   const checkboxes = document.querySelectorAll('.check_box input');
 
