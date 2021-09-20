d3711fa25d671d4091ebaaf1cb13617052839bc4
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-20 17:08:12 +0300

    index.js code restructure

--- src/js/index.js
+++ src/js/index.js
@@ -1,89 +1,52 @@
 import { initEditing } from './editing.js';
 import { initFilters } from './filter.js';
 import { people } from './mock.js';
-import { generatePagination } from './pagination.js';
 import { generateTable, updateRow } from './table.js';
 import { initToggleColumns } from './toggleColumns.js';
 
-const initApp = ({
-  currentPage = 1,
-  rowsPerPage = 10,
-  data,
-  toggleColumns,
-  edit,
-  containerElement,
-  filters,
-}) => {
+const initApp = ({ currentPage = 1, rowsPerPage = 10, data, containerElement }) => {
   const state = {
     hiddenColumns: [],
+    filteredTable: [],
     currentPage,
     rowsPerPage,
-    toggleColumns: {
-      selector: '',
-      enabled: false,
-    },
-    filters: filters,
     data: [...data],
     containerElement,
   };
 
   state.containerElement.appendChild(generateTable({ ...state }));
 
-  if (toggleColumns.enabled) {
-    initToggleColumns(state, hiddenColumns => {
-      state.hiddenColumns = hiddenColumns;
-      state.containerElement.innerHTML = '';
+  initToggleColumns(state, hiddenColumns => {
+    state.hiddenColumns = hiddenColumns;
+    state.containerElement.innerHTML = '';
 
-      state.containerElement.appendChild(generateTable({ ...state }));
-      if (edit.enabled) {
-        initEditing(state, (row, data) => {
-          updateRow(row, data, hiddenColumns);
-        });
-      }
-    });
-  }
-
-  if (filters.enabled) {
-    initFilters();
-  }
-
-  if (edit.enabled) {
-    initEditing(state, (row, data, hiddenColumns) => {
+    state.containerElement.appendChild(generateTable({ ...state }));
+    initEditing(state, (row, data) => {
       updateRow(row, data, hiddenColumns);
     });
-  }
+  });
 
-  generatePagination(state, ({ data, rowsPerPage, currentPage, hiddenColumns }) => {
+  initFilters({ ...state }, row => {
+    updateTbody(row);
+  });
+
+  initEditing(state, (row, data, hiddenColumns) => {
+    updateRow(row, data, hiddenColumns);
+  });
+
+  initPagination(state, ({ data, rowsPerPage, currentPage, hiddenColumns }) => {
     state.containerElement.innerHTML = '';
 
     state.containerElement.appendChild(
       generateTable({ data, rowsPerPage, currentPage, hiddenColumns }),
     );
-    if (edit.enabled) {
-      initEditing(state, (row, data) => {
-        updateRow(row, data, hiddenColumns);
-      });
-    }
+    initEditing(state, (row, data) => {
+      updateRow(row, data, hiddenColumns);
+    });
   });
 };
 
 initApp({
   data: people,
-  toggleColumns: {
-    enabled: true,
-    selector: '#toggleColumns',
-  },
-  filters: {
-    enabled: true,
-    values: {
-      name: null,
-      phone: null,
-      about: null,
-      color: null,
-    },
-  },
-  edit: {
-    enabled: true,
-  },
   containerElement: document.querySelector('#container'),
 });
